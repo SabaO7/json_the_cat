d72ccbcbaf58e6c7ef8073f98622c9e67b451c3a
Saba Oji <sabaoji@gmail.com>
2023-05-22 16:56:41 -0400

    refactoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,21 @@
 const request = require('request');
 
-const breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length > 0) {
-      const breed = data[0];
-      console.log(breed.description);
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     } else {
-      console.log('Breed not found!');
+      const data = JSON.parse(body);
+      if (data.length > 0) {
+        const breed = data[0];
+        callback(null, breed.description);
+      } else {
+        callback('Breed not found!', null);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
